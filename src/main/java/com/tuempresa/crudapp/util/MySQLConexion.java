1758bf958f898156283fc1893ffe543920753fdc
Alexis Quiroga <quirogaalee@gmail.com>
2023-12-17 20:19:48 -0300

    Conectividad DB

--- src/main/java/com/tuempresa/crudapp/util/MySQLConexion.java
+++ src/main/java/com/tuempresa/crudapp/util/MySQLConexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/proyectocrud";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Spesia01";
+    private static final String URL = "jdbc:mysql://localhost:3306/tubasededatos";
+    private static final String USER = "tuusuario";
+    private static final String PASSWORD = "tucontraseña";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
